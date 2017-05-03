d635e96f0ec01263e474e51bdacc69b4fa27d2ea
강인지 <ruci06@gmail.com>
2017-05-03 17:57:06 +0900

    퍼블리싱 내용 적용

--- maybe-front/src/App.js
+++ maybe-front/src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // 로딩뷰를 만들고
+    }
+  }
+  componentWillMount() {
+    //컴포넌트가 마운트 되기 전에 실행
+    //AJAX콜 해서 데이터 가져오기
+    axios.get('localhost:4000/book/'+this.props.bid , (result) => {
+      this.setState({book:result});
+    })
+
+  }
+  componentWillUpdate() {
+    //컴포넌트가 업데이트 되기 전에 실행
+  }
+  componentDidMount() {
+    //컴포너트가 마운트 된 직후에 실행
+  }
+  componentDidUpdate() {
+    //컴포넌트가 업데이트 된 직후에 실행
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="book-info-background">
+          <div className="container">
+              <div className="book-info-inner">
+                  <img src={logo} className="book-info-thumbnail"/>
+              </div>
+              <div className="book-info-inner">
+                  <h3>책 제목</h3>
+                  <div className="book-info-list">
+                      <span>저자</span>
+                      <a href="#">{this.state.book.author.name}</a>
+                  </div>
+                  <div className="book-info-list">
+                      <span>출판사</span>
+                      <span>출판사명</span>
+                  </div>
+                  <div className="book-info-list">
+                      <span>출간일</span>
+                      <span>출판사명</span>
+                  </div>
+                  <div className="book-info-list">
+                      <span>정가</span>
+                      <span>출판사명</span>
+                  </div>
+              </div>
+          </div>
       </div>
     );
   }
